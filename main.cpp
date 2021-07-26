b4057b52447168530f828b92303075eeb1c491ca
k4-100 <k4-100@protonmail.com>
2021-07-26 18:10:43 +0200

    Finished basic sample generation

--- main.cpp
+++ main.cpp
@@ -1,19 +1,41 @@
 #include <iostream>
 //std::ofstream
 #include <fstream>
-// to_string()
+// std::to_string()
 #include <string>
+// std::mt19937, std::uniform_int_distribution
+#include <random>
+// std::setprecision()
+#include <iomanip>
+
+
 
 int main()
 {
-    std::ofstream writer("sample.txt");
+    // writes data into a file (creates it, if necessary)
+    std::ofstream writer( "sample.txt" );
+
+    // random number generator
+    std::mt19937 generator;
+    generator.seed( std::time( 0 ) );
+
+    // uniform limiting range of possible values
+    std::uniform_real_distribution< float > dice( 0, 20 );
 
     // buffer for storing loop output
-    std::string buffer = "";
-    for (uint8_t i = '\000'; i < 200; i++)
+    std::stringstream buffer;
+    
+    // maximal index ( can be no bigger than 365 )
+    const uint16_t maxIndex = uint16_t( 10 % 366);
+
+    for (uint16_t i = 0; i < maxIndex; i++)
     {
-        buffer += std::to_string(i) + '\n';
+        float generatedValue =  ( dice( generator ) );
+
+        // stream data into buffer, while also ensuring that decimal places will be displayed with decimal place
+        // and that the precision will be up to 1 decimal place
+        buffer <<  std::fixed << std::setprecision( 1 ) <<  generatedValue  <<  '\n';
     }
 
-    std::cout << buffer << '\n';
+    writer << buffer.str();
 }
